641bdee21ba7d11a0049c3bea85c424a26069cb8
CodingOwl0 <add.20@web.de>
2026-05-28 15:18:39 +0200

    Add files via upload

---
+++ CMDCrypt/Crypt.vb
@@ -0,0 +1,110 @@
+﻿Imports System.Diagnostics.Eventing.Reader
+Imports System.IO
+Imports System.Security.Cryptography
+Imports System.Text
+Module Crypt
+
+    Public Function Encrypt(input As String, key As String) As String
+        Dim AES As New RijndaelManaged
+        Dim SHA256 As New SHA256Cng
+        Dim ciphertext As String
+        Try
+            AES.GenerateIV()
+            AES.Key = SHA256.ComputeHash(Encoding.ASCII.GetBytes(key))
+            AES.Mode = CipherMode.CBC
+            Dim DESEncrypter As ICryptoTransform = AES.CreateEncryptor
+            Dim Buffer As Byte() = Encoding.ASCII.GetBytes(input)
+            ciphertext = Convert.ToBase64String(DESEncrypter.TransformFinalBlock(Buffer, 0, Buffer.Length))
+
+            Return Convert.ToBase64String(AES.IV) & Convert.ToBase64String(DESEncrypter.TransformFinalBlock(Buffer, 0, Buffer.Length))
+
+        Catch ex As Exception
+            Console.WriteLine("[1004] Could not encrypt string " + ex.ToString & vbCrLf)
+        End Try
+        Return ""
+    End Function
+
+
+
+    Public Function Decrypt(ciphertext As String, key As String) As String
+        Dim AES As New RijndaelManaged
+        Dim SHA256 As New SHA256Cng
+        Dim plaintext As String
+        Dim iv As String
+        Try
+            Dim ivct = ciphertext.Split({"=="}, StringSplitOptions.None)
+            iv = ivct(0) & "=="
+            ciphertext = If(ivct.Length = 3, ivct(1) & "==", ivct(1))
+
+            AES.Key = SHA256.ComputeHash(ASCIIEncoding.ASCII.GetBytes(key))
+            AES.IV = Convert.FromBase64String(iv)
+            AES.Mode = CipherMode.CBC
+            Dim DESDecrypter As ICryptoTransform = AES.CreateDecryptor
+            Dim Buffer As Byte() = Convert.FromBase64String(ciphertext)
+            plaintext = ASCIIEncoding.ASCII.GetString(DESDecrypter.TransformFinalBlock(Buffer, 0, Buffer.Length))
+            Return plaintext
+        Catch ex As Exception
+            Console.WriteLine("[1005] Could not decrypt message- Maybe the key is invalid." + ex.ToString & vbCrLf)
+        End Try
+        Return ""
+    End Function
+
+
+
+
+
+    Public Function AESEncryptFile(plainFilePath As String, encryptedFilePath As String, EncryptionKey As String, SaltValue As String) As Boolean ', IV As String
+        Try
+            Dim pdb As New Rfc2898DeriveBytes(EncryptionKey, New Byte() {&H49, &H76, &H61, &H6E, &H20, &H4D,
+                     &H65, &H64, &H76, &H65, &H64, &H65, &H76})
+            'Dim Key = pdb.GetBytes(32)
+            'IV = pdb.GetBytes(16).ToString
+            Dim initVectorBytes As Byte() = pdb.GetBytes(16) 'Encoding.ASCII.GetBytes(IV)
+            Dim saltValueBytes As Byte() = Encoding.ASCII.GetBytes(SaltValue)
+            Dim k1 As New Rfc2898DeriveBytes(EncryptionKey, saltValueBytes, 100)
+            Dim symmetricKey As New AesManaged With {
+                    .KeySize = 256,
+                    .Mode = CipherMode.CBC
+                }
+            Dim encryptor As ICryptoTransform = symmetricKey.CreateEncryptor(k1.GetBytes(16), initVectorBytes)
+            Using plain As FileStream = File.Open(plainFilePath, FileMode.Open, FileAccess.Read, FileShare.Read)
+                Using encrypted As FileStream = File.Open(encryptedFilePath, FileMode.Create, FileAccess.Write, FileShare.None)
+                    Using cs As New CryptoStream(encrypted, encryptor, CryptoStreamMode.Write)
+                        plain.CopyTo(cs)
+                    End Using
+                End Using
+            End Using
+            Return True
+        Catch ex As Exception
+            Console.WriteLine("[1006] Could not encrypt file: " + ex.ToString & vbCrLf)
+        End Try
+        Return False
+    End Function
+
+    Public Function AESDecryptFile(encryptedFilePath As String, plainFilePath As String, DecryptionKey As String, SaltValue As String) As Boolean ', IV As String
+        Try
+            Dim pdb As New Rfc2898DeriveBytes(DecryptionKey, New Byte() {&H49, &H76, &H61, &H6E, &H20, &H4D,
+                     &H65, &H64, &H76, &H65, &H64, &H65, &H76})
+            Dim initVectorBytes As Byte() = pdb.GetBytes(16)  'Encoding.ASCII.GetBytes(IV)
+            Dim saltValueBytes As Byte() = Encoding.ASCII.GetBytes(SaltValue)
+            Dim k1 As New Rfc2898DeriveBytes(DecryptionKey, saltValueBytes, 100)
+            Dim symmetricKey As New AesManaged With {
+                    .KeySize = 256,
+                    .Mode = CipherMode.CBC
+                }
+            Dim decryptor As ICryptoTransform = symmetricKey.CreateDecryptor(k1.GetBytes(16), initVectorBytes)
+            Using plain As FileStream = File.Open(plainFilePath, FileMode.Create, FileAccess.Write, FileShare.None)
+                Using encrypted As FileStream = File.Open(encryptedFilePath, FileMode.Open, FileAccess.Read, FileShare.Read)
+                    Using cs As New CryptoStream(plain, decryptor, CryptoStreamMode.Write)
+                        encrypted.CopyTo(cs)
+                    End Using
+                End Using
+            End Using
+            Return True
+        Catch ex As Exception
+            Console.WriteLine("[1007] Could not decrypt file: " + ex.ToString & vbCrLf)
+        End Try
+        Return False
+    End Function
+
+End Module
---
+++ CMDCrypt/Main.vb
@@ -0,0 +1,92 @@
+﻿Imports System.IO
+Module Main
+    Private output As String = ""
+
+    Sub Main(args As String())
+        If args.Length >= 3 Then
+            Select Case args(0).ToLower
+                Case "-f"          '--------> Encrypt file
+                    If File.Exists(args(1)) Then
+                        Dim fI As New FileInfo(args(1))
+                        If AESEncryptFile(fI.FullName, args(2), args(3), args(4)) Then
+                            Console.WriteLine("(i) Successfully encrypted" & vbCrLf)
+                        Else
+                            Console.WriteLine("(!) Could not encrypt" & vbCrLf)
+                        End If
+                    Else
+                        Console.WriteLine("(!) File not found" & vbCrLf)
+                    End If
+
+                Case "-fd"          '--------> Decrypt file
+                    If File.Exists(args(1)) Then
+                        If AESDecryptFile(args(1), args(2), args(3), args(4)) Then
+                            Console.WriteLine("(i) Successfully decrypted" & vbCrLf)
+                        Else
+                            Console.WriteLine("(!) Could not decrypt" & vbCrLf)
+                        End If
+                    Else
+                        Console.WriteLine("(!) File not found")
+                    End If
+
+                Case "-s"          '--------> Encrypt string
+                    output = Encrypt(args(1), args(2) + args(3))
+                    Console.WriteLine(output)
+
+                Case "-sd"          '--------> Decrypt string
+                    output = Decrypt(args(1), args(2) + args(3))
+                    Console.WriteLine(output)
+
+                Case "-d"          '--------> Encrypt whole dir
+                    If Directory.Exists(args(1)) Then
+                        For Each file As String In Directory.GetFiles(args(1))
+                            Dim finfo As New FileInfo(file)
+                            Console.WriteLine("> Encrypting " + file & vbCrLf)
+                            If AESEncryptFile(file, Path.Combine(args(2), finfo.Name), args(3), args(4)) Then
+                                Console.WriteLine("(i) Successfully encrypted " + file & vbCrLf)
+                            Else
+                                Console.WriteLine("(!) Could not encrypt " + file & vbCrLf)
+                            End If
+                        Next
+                        Console.WriteLine("(i) Done")
+                    Else
+                        Console.WriteLine("(!) Directory " + args(0) + " Not found" & vbCrLf)
+                    End If
+
+                Case "-dd"          '--------> Decrypt whole dir
+                    If Directory.Exists(args(1)) Then
+                        For Each file As String In Directory.GetFiles(args(1))
+                            Dim finfo As New FileInfo(file)
+                            Console.WriteLine("> Decrypting " + file & vbCrLf)
+                            If AESDecryptFile(file, Path.Combine(args(2), finfo.Name), args(3), args(4)) Then
+                                Console.WriteLine("(i) Successfully decrypted " + file & vbCrLf)
+                            Else
+                                Console.WriteLine("(!) Could not decrypt " + file & vbCrLf)
+                            End If
+                        Next
+                        Console.WriteLine("(i) Done")
+                    Else
+                        Console.WriteLine("(!) Directory " + args(0) + " Not found")
+                    End If
+            End Select
+        Else
+            DisplayHelp()
+        End If
+    End Sub
+    Sub DisplayHelp()
+        Console.WriteLine("CMDCrypt - Encrypt files and strings with AES CBC" & vbCrLf + vbCrLf)
+        Console.WriteLine("(c) Luca Franziskowski, All rights reserved. Build in 2022" & vbCrLf)
+        Console.WriteLine("Parameters:")
+        Console.WriteLine("-f  : Encrypt a file   -s  : Encrypt a string     -d  : Encrypt whole directory")
+        Console.WriteLine("-fd : Decrypt a file   -sd : Decrypt a string     -dd : Decrypt whole directory" & vbCrLf)
+        Console.WriteLine("-> Please note: if you are encrypting/decrypting a whole directory, " & vbCrLf +
+                          "you need to choose a source- and destination-directory." & vbCrLf +
+                          "-> Your salt needs to be at least 8 Bytes!")
+        Console.WriteLine("Syntax:")
+        Console.WriteLine("cmdCrypt.exe <Parameter> ""Source-Filename"" ""Output-Filename"" ""Password"" ""Salt""" & vbCrLf)
+        Console.WriteLine("Example:")
+        Console.WriteLine("cmdCrypt.exe -f ""C:\Users\myFile.txt"" ""D:\Example\encrypted.txt"" abcde123fg IamASalt")
+        Console.WriteLine("cmdCrypt.exe -dd ""C:\MySourceFolder"" ""D:\MyDestinationFolder"" abcde123fg Thisis1asalt")
+        Console.Read()
+    End Sub
+
+End Module
